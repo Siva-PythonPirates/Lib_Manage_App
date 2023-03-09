da2cc86f0994d95622d8028f9cf44b986265fd06
unknown <sivaharicr7@gmail.com>
2023-03-09 19:06:56 +0530

    Loading screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,96 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:lib_management/view_model/app_provider.dart';
 import 'package:lib_management/views/login_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    Changes(
-      child: const MaterialApp(
-        home: Login(),
-        debugShowCheckedModeBanner: false,
-        title: 'REC Library',
-      ),
+    const MaterialApp(
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+      title: 'REC Library',
     ),
   );
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _translateAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 2000),
+    );
+    _translateAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeIn,
+      ),
+    );
+    Timer(
+      const Duration(seconds: 3),
+          () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => const Login(),
+          ),
+        );
+      },
+    );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Center(
+            child: FadeTransition(
+              opacity: _translateAnimation,
+              child: SizedBox(
+                height: screenSize.height * 0.5,
+                width: screenSize.width * 0.5,
+                child: Image.asset('assets/images/rec_logo.jpg'),
+              ),
+            ),
+          ),
+          Center(
+            child: FadeTransition(
+              opacity: _translateAnimation,
+              child: Text(
+                '\n\n\n\n\n\n\nREC_LIBRARY',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/views/book_details.dart
+++ lib/views/book_details.dart
@@ -1,46 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class BookDetails extends StatelessWidget {
+class BookDetails extends StatefulWidget {
   final Map<String, dynamic> book;
 
   const BookDetails({Key? key, required this.book}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final Size screenSize = MediaQuery.of(context).size;
+  _BookDetailsState createState() => _BookDetailsState();
+}
 
-    return Scaffold(
+class _BookDetailsState extends State<BookDetails> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    // Simulate an asynchronous operation to fetch book details.
+    // After the operation is complete, set _isLoading to false to
+    // show the book details screen.
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery
+        .of(context)
+        .size;
+
+    return _isLoading
+        ? _buildLoadingScreen()
+        : Scaffold(
       appBar: AppBar(
-        title: Text(book['title'] ?? 'BOOK TITLE'),
-        backgroundColor: Colors.pink[200],
-        toolbarTextStyle: Theme.of(context)
+        title: Text(widget.book['title'] ?? 'BOOK TITLE'),
+        textTheme: Theme
+            .of(context)
             .textTheme
             .copyWith(
-              headline6: TextStyle(color: Colors.black),
-            )
-            .bodyText2,
-        titleTextStyle: Theme.of(context)
-            .textTheme
-            .copyWith(
-              headline6: TextStyle(color: Colors.black),
-            )
-            .headline6,
+          headline6: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.pink.shade200,
       ),
       body: Container(
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/login_bg.jpg"),
             fit: BoxFit.cover,
           ),
         ),
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: screenSize.width * 0.2, vertical: 16),
+          padding: EdgeInsets.symmetric(
+              horizontal: screenSize.width * 0.2, vertical: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: screenSize.height * 0.3,
-                width: MediaQuery.of(context).size.width,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
                 child: Image.network(
                   'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
                   fit: BoxFit.cover,
@@ -48,38 +71,186 @@ class BookDetails extends StatelessWidget {
               ),
               const SizedBox(height: 16),
               Text(
-                'Title: ${book['title'] ?? 'BOOK TITLE'}',
-                style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                      color: Colors.white,
-                    ),
+                'Title: ${widget.book['title'] ?? 'BOOK TITLE'}',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headline6
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
               ),
               const SizedBox(height: 8),
               Text(
-                'Author: ${book['author'] ?? 'XXX'}',
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      color: Colors.white,
-                    ),
+                'Author: ${widget.book['author'] ?? 'XXX'}',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
               ),
               const SizedBox(height: 8),
               Text(
-                'Department: ${book['department'] ?? 'XXX'}',
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      color: Colors.white,
-                    ),
+                'Department: ${widget.book['department'] ?? 'XXX'}',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
               ),
               const SizedBox(height: 16),
               Text(
                 'Description:',
-                style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      color: Colors.white,
-                    ),
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
               ),
               const SizedBox(height: 8),
               Text(
-                book['description'] ?? 'No description available',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                      color: Colors.white,
-                    ),
+                widget.book['description'] ??
+                    'No description available',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .bodyText1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoadingScreen() {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SpinKitDualRing(
+              color: Colors.pink,
+              size: 100,
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Loading Your Book',
+              style: Theme.of(context).textTheme.subtitle1,
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget Build(BuildContext context) {
+    final Size screenSize = MediaQuery
+        .of(context)
+        .size;
+    return _isLoading
+        ? _buildLoadingScreen()
+        : Scaffold(
+      appBar: AppBar(
+        title: Text(widget.book['title'] ?? 'BOOK TITLE'),
+        textTheme: Theme
+            .of(context)
+            .textTheme
+            .copyWith(
+          headline6: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.pink.shade200,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/login_bg.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: screenSize.width * 0.2, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: screenSize.height * 0.3,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                child: Image.network(
+                  'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'Title: ${widget.book['title'] ?? 'BOOK TITLE'}',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .headline6
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Author: ${widget.book['author'] ?? 'XXX'}',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Department: ${widget.book['department'] ?? 'XXX'}',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                'Description:',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                widget.book['description'] ??
+                    'No description available',
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .bodyText1
+                    ?.copyWith(
+                  color: Colors.white,
+                ),
               ),
             ],
           ),
