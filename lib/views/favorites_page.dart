0299fd19da9d76690fc388892a577c93987332fa
soniyav20 <soniyav2003@gmail.com>
2023-03-12 23:17:01 +0530

    Fav Page UI Done

--- lib/views/favorites_page.dart
+++ lib/views/favorites_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:lib_management/services/app_services.dart';
 import 'package:lib_management/services/app_services_impl.dart';
 import 'package:lib_management/view_model/app_provider.dart';
-import 'package:lib_management/views/book_details.dart';
 import 'package:provider/provider.dart';
 
 import '../services/app_constants.dart';
@@ -36,7 +35,7 @@ class _MyFavoritesPageState extends State<MyFavoritesPage> {
             Expanded(
               child: Scrollbar(
                 child: Padding(
-                  padding: EdgeInsets.only(top: imp.getSize(context, 200)),
+                  padding: EdgeInsets.only(top: imp.getSize(context, 180)),
                   child: favoriteBooks.isEmpty
                       ? Center(
                           child: Text('No favorite books yet'),
@@ -59,26 +58,130 @@ class _MyFavoritesPageState extends State<MyFavoritesPage> {
                                 context.read<MyModel>().removeFromFavorites(
                                     book["title"] ?? "null");
                               },
-                              child: Container(
-                                color: Colors.white,
-                                child: ListTile(
-                                  leading: Image.network(
-                                    book['image'] ?? "",
-                                    height: 100,
-                                    width: 80,
-                                    fit: BoxFit.cover,
-                                  ),
-                                  title: Text(book['title'] ?? ''),
-                                  subtitle: Text("Author"),
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            BookDetails(book: book),
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: imp.getSize(context, 16),
+                                    vertical: imp.getSize(context, 10)),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(
+                                      imp.getSize(context, 10),
+                                    ),
+                                    color:
+                                        const Color.fromRGBO(255, 255, 255, 1),
+                                    boxShadow: const [
+                                      BoxShadow(
+                                        color:
+                                            Color.fromARGB(255, 118, 23, 182),
+                                        blurRadius: 5,
+                                        spreadRadius: 7,
                                       ),
-                                    );
-                                  },
+                                    ],
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(
+                                        imp.getSize(context, 10)),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          margin: EdgeInsets.only(
+                                              left: imp.getSize(context, 8)),
+                                          height: imp.getSize(context, 100),
+                                          width: imp.getSize(context, 80),
+                                          color: Colors.white,
+                                          child: Image(
+                                            loadingBuilder: ((context, child,
+                                                loadingProgress) {
+                                              if (loadingProgress == null) {
+                                                return child;
+                                              } else {
+                                                return Expanded(
+                                                  child: Center(
+                                                    child:
+                                                        CircularProgressIndicator(
+                                                      color: bgColor,
+                                                    ),
+                                                  ),
+                                                );
+                                              }
+                                            }),
+                                            image: const NetworkImage(
+                                                'https://edit.org/images/cat/book-covers-big-2019101610.jpg'),
+                                            fit: BoxFit.fill,
+                                          ),
+                                        ),
+                                        // CachedNetworkImage(
+                                        //   imageUrl: books[index]['image']!,
+                                        //   imageBuilder: (context, imageProvider) => Container(
+                                        //     width: double.infinity,
+                                        //     height: imp.getSize(context, 140),
+                                        //     decoration: BoxDecoration(
+                                        //       image: DecorationImage(
+                                        //         image: imageProvider,
+                                        //         fit: BoxFit.cover,
+                                        //       ),
+                                        //     ),
+                                        //   ),
+                                        //   placeholder: (context, url) {
+                                        //     return SizedBox(
+                                        //       height: imp.getSize(context, 100),
+                                        //       width: imp.getSize(context, 80),
+                                        //       child: Center(
+                                        //         child: CircularProgressIndicator(
+                                        //           color: bgColor,
+                                        //         ),
+                                        //       ),
+                                        //     );
+                                        //   },
+                                        //   errorWidget: (context, url, error) => const Icon(Icons.error),
+                                        // ),
+                                        Padding(
+                                          padding: EdgeInsets.all(
+                                              imp.getSize(context, 8)),
+                                          child: Column(
+                                            children: [
+                                              SizedBox(
+                                                width:
+                                                    imp.getSize(context, 200),
+                                                child: Center(
+                                                  child: Text(
+                                                    book['title'] ??
+                                                        "BOOK TITLE",
+                                                    textAlign: TextAlign.center,
+                                                    style: const TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.bold),
+                                                  ),
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: EdgeInsets.only(
+                                                    top: imp.getSize(
+                                                        context, 8)),
+                                                child: SizedBox(
+                                                  width:
+                                                      imp.getSize(context, 200),
+                                                  child: Column(
+                                                    // mainAxisAlignment: MainAxisAlignment.center,
+                                                    children: [
+                                                      Text(
+                                                        'AUTHOR: ${book['Author'] ?? "XXX"}',
+                                                        textAlign:
+                                                            TextAlign.center,
+                                                      ),
+                                                      Text(
+                                                        'DEPT: ${book['Department'] ?? "XXX"}',
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
                                 ),
                               ),
                             );
